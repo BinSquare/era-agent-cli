e9afed08ac896264d05288670f0c8d5eb22de497
BinBin He <hebinsquared@gmail.com>
2025-12-04 15:42:50 -0800

    feat: pinning versions of dependencies.

---
+++ Formula/krunvm@0.2.4.rb
@@ -0,0 +1,29 @@
+class KrunvmAT024 < Formula
+  desc "Manage lightweight VMs created from OCI images"
+  homepage "https://github.com/slp/krunvm"
+  url "https://github.com/containers/krunvm/archive/refs/tags/v0.2.4.tar.gz"
+  sha256 "d71467fa62c43141334f8d40b81aa7297ddd6e68d5dda9e1202b085e6d81655c"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
+    sha256 cellar: :any, arm64_sequoia: "56a6302357c4479ac920b8bf233a43e0bcc134c9c809414a8c676d853839b58b"
+  end
+
+  depends_on "asciidoctor" => :build
+  depends_on "rust" => :build
+  # We depend on libkrun, which only supports Hypervisor.framework on arm64
+  depends_on arch: :arm64
+  depends_on "buildah"
+  depends_on "libkrun@1.15.1"
+
+  def install
+    system "make"
+    bin.install "target/release/krunvm"
+    man1.install Dir["target/release/build/krunvm-*/out/*.1"]
+  end
+
+  test do
+    system "krunvm", "--version"
+  end
+end
---
+++ Formula/libkrun@1.15.1.rb
@@ -0,0 +1,37 @@
+class LibkrunAT1151 < Formula
+  desc "Dynamic library providing KVM-based process isolation capabilities"
+  homepage "https://github.com/containers/libkrun"
+  version "1.15.1"
+  url "https://github.com/containers/libkrun/releases/download/v1.15.1/libkrun-1.15.1-prebuilt-aarch64.tar.gz"
+  sha256 "0ca2999968dbc469e9949d5a61f4399e339a51f3db7ff356aba3b8f13fc4c075"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
+    sha256 cellar: :any, arm64_sequoia: "e4eb00e58f84e95caef93d36c403b4dd134fa140f2b884bea4c0d62c076afa2d"
+  end
+
+  depends_on "rust" => :build
+  # Upstream only supports Hypervisor.framework on arm64
+  depends_on arch: :arm64
+  depends_on "dtc"
+  depends_on "libkrunfw"
+
+  def install
+    system "make", "BLK=1", "NET=1"
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <libkrun.h>
+      int main()
+      {
+         int c = krun_create_ctx();
+         return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lkrun", "-o", "test"
+    system "./test"
+  end
+end
